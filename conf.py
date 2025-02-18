2e184a443063424ac1e27b5738c2c80fac12c8f2
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2025-02-18 18:48:23 +0100

    No need to set html_theme_path anymore (#8)

--- conf.py
+++ conf.py
@@ -169,10 +169,7 @@ todo_include_todos = False
 # a list of builtin themes.
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
     html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
     # Add any paths that contain custom static files (such as style sheets) here,
     # relative to this directory. They are copied after the builtin static files,
